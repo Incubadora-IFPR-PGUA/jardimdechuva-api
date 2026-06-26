7d07fe903c7a986ed508e58f665bdd58037fcc1b
Kayure <52473139+Kayure@users.noreply.github.com>
2026-06-26 16:46:57 -0300

    Feat:,adicionado parametros max e min e tipo de sensor

--- app/Controllers/Http/SensorController.ts
+++ app/Controllers/Http/SensorController.ts
@@ -1,6 +1,7 @@
 import { DateTime } from 'luxon'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Sensor from 'App/Models/Sensor'
+import ConfiguracaoSensor from 'App/Models/ConfiguracaoSensor'
 
 export default class SensorController {
   public async index({ response }: HttpContextContract) {
@@ -17,7 +18,19 @@ export default class SensorController {
       'idDispositivo', 'idTipoSensor', 'nome',
       'mqttTopicoLeitura', 'imagemUrl', 'localizacao'
     ])
+    const configData = request.only(['valorMin', 'valorMax'])
+    
     const sensor = await Sensor.create(data)
+
+    if (configData.valorMin !== undefined || configData.valorMax !== undefined) {
+      await ConfiguracaoSensor.create({
+        idSensor: sensor.idSensor,
+        valorMin: configData.valorMin !== '' ? Number(configData.valorMin) : null,
+        valorMax: configData.valorMax !== '' ? Number(configData.valorMax) : null,
+        alertaAtivo: true
+      })
+    }
+
     return response.created(sensor)
   }
 
@@ -38,9 +51,29 @@ export default class SensorController {
     const sensor = await Sensor.findOrFail(params.id)
     sensor.merge(request.only([
       'nome', 'estadoAtual', 'valorAtual',
-      'atualizadoEm', 'imagemUrl', 'localizacao'
+      'atualizadoEm', 'imagemUrl', 'localizacao', 'idTipoSensor'
     ]))
     await sensor.save()
+
+    const configData = request.only(['valorMin', 'valorMax'])
+    if (configData.valorMin !== undefined || configData.valorMax !== undefined) {
+      const config = await ConfiguracaoSensor.findBy('idSensor', sensor.idSensor)
+      if (config) {
+        config.merge({
+          valorMin: configData.valorMin !== '' && configData.valorMin !== null ? Number(configData.valorMin) : null,
+          valorMax: configData.valorMax !== '' && configData.valorMax !== null ? Number(configData.valorMax) : null,
+        })
+        await config.save()
+      } else {
+        await ConfiguracaoSensor.create({
+          idSensor: sensor.idSensor,
+          valorMin: configData.valorMin !== '' && configData.valorMin !== null ? Number(configData.valorMin) : null,
+          valorMax: configData.valorMax !== '' && configData.valorMax !== null ? Number(configData.valorMax) : null,
+          alertaAtivo: true
+        })
+      }
+    }
+
     return response.ok(sensor)
   }
 
